2da2ae6d25a701a54ea2fd05a11ac3aee93d5860
deathlezz <78265796+deathlezz@users.noreply.github.com>
2021-05-29 14:07:50 +0200

    Add files via upload

---
+++ formula.swift
@@ -0,0 +1,42 @@
+//
+//  Countdown Timer (formula)
+//
+//  Created by deathlezz on 29/05/2021.
+//
+
+
+import Foundation
+
+
+// set the dates
+let futureDate: Date = {
+    
+    // set the day in a future
+    let future = DateComponents(year: 2022, month: 7, day: 0, hour: 0, minute: 0)
+    
+    // set the current date
+    return Calendar.current.date(from: future)!
+}()
+
+
+// set the countdown
+var countdown: DateComponents {
+    
+    // change the seconds to hours, minutes and days
+    return Calendar.current.dateComponents([.day, .hour, .minute, .second], from: Date(), to: futureDate)
+}
+
+
+// set the variable to our remaining time
+let days = countdown.day!
+let hours = countdown.hour!
+let minutes = countdown.minute!
+let seconds = countdown.second!
+
+
+// set the countdown label
+let countDownLabel = "\(days) Days, \(hours) Hours, \(minutes) Minutes, \(seconds) Seconds"
+
+
+// final output
+print(countDownLabel)
