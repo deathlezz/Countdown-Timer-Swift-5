8cdff25a28e82f2da88aeb31e501f087d3b4a1ce
deathlezz <78265796+deathlezz@users.noreply.github.com>
2021-07-28 12:42:41 +0200

    Update formula2.swift

--- formula2.swift
+++ formula2.swift
@@ -6,24 +6,19 @@
 
 import Foundation
 
+// Set event date
+let eventDate = DateComponents(year: 2021, month: 7, day: 28, hour: 12, minute: 35)
+
 // Set current date
 let date = Date()
 let userCalendar = Calendar.current
 let components = userCalendar.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date)
 let currentDate = userCalendar.date(from: components)!
         
-// Set event date
-var eventDate = DateComponents()
-eventDate.year = 2022
-eventDate.month = 5
-eventDate.day = 25
-eventDate.hour = 0
-eventDate.minute = 0
-        
-// Convert eventDate to the user's calendar
+// Convert eventDate's calendar to the user's calendar
 let convertedEventDate = userCalendar.date(from: eventDate)!
         
-// Change the seconds to days, hours and minutes
+// Change dates' diference to days, hours, minutes and seconds
 let timeLeft = userCalendar.dateComponents([.day, .hour, .minute, .second], from: currentDate, to: convertedEventDate)
 
 // Set the countdown label
